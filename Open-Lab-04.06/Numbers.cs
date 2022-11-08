e62d5e14068ec4bb6058a9ff72d21744176173e3
Lucián Švancár <svancarlucian@gmail.com>
2022-11-08 09:32:41 +0100

    Add using system.Linq, and return even ones

--- Open-Lab-04.06/Numbers.cs
+++ Open-Lab-04.06/Numbers.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
+
 namespace Open_Lab_04._06
 {
     public class Numbers
     {
         public int[] NoOdds(int[] numbers)
         {
-            throw new NotImplementedException();
+            return numbers.Where(x=> x%2 == 0).ToArray();
         }
     }
+
 }
